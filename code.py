8042c8ac5ff53055f110f7b947d7af187ebe4790
Dj1ngisKhan <halloklish@gmail.com>
2025-01-11 18:40:57 +0100

    Update code.py
    raderade några av tweets alternativen

--- code.py
+++ code.py
@@ -16,23 +16,10 @@ client = OpenAI()
 
 
 #GPT QUESTIONS:
-tweets = ["Make a tweet about some a interesting theory about the universe that is less then 280 characters, "
-          "only include the content.",
-          "Make a tweet about a interesting theory about finance in the future, like 100 years from today, "
-          "only include the content, have it less the 280 characters long",
-          "Make a tweet about a interesting conspiracy that I should post on twitter, make it less then 280 characters, "
-          "also ask other users what their believes on this is? ChatGPT",
-          "Make a short story less then 280 characters long, have a surprising plot twist at the end",
-          "Make a short horror story less then 280 characters long, have a surprising plot tiwst at the end",
-          "Make a short story about the universe less then 280 characters long, have a surprising plot tiwst at the end",
-          "Make a dad joke as a tweet",
-          "Relatable Experiences: Share humorous takes on common experiences. (Just tried to use my car keys to unlock "
-          "my house. If anyone needs me, I'll be in bed reassessing my life choices.)",
-          "Pet Antics: Pets are a goldmine for humor. My cat just stared at me for 10 minutes then knocked a "
-          "glass off the table. Her message was clear: 'I own you.'",
-          "Imaginary Scenarios: Create absurd hypothetical situations. Imagine if at the end of a marathon, they "
-          "handed out free pizzas instead of medals. I bet record times would shatter."
-          ]
+tweets = ["Make a tweet about some a interesting theory about the universe that is less then 280 characters, 
+"only include the content.",
+"Make a tweet about a interesting theory about finance in the future, like 100 years from today"]
+          
 r_message = random.choice(tweets)
 
 
